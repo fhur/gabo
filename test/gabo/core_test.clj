88644ee7a536141f3cc75f54b472db5e5a2088c8
fhur <fernando@authy.com>
2015-06-04 10:01:25 -0500

    Added tests

--- test/gabo/core_test.clj
+++ test/gabo/core_test.clj
@@ -2,6 +2,64 @@
   (:require [clojure.test :refer :all]
             [gabo.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest is-functions
+  (testing "is-literal"
+    (is (is-literal [:literal "foo"])))
+  (testing "is-symbol"
+    (is (is-symbol [:symbol "arg"])))
+  (testing "is-iter-init"
+    (is (is-iter-init [:iter-init "bar"])))
+  (testing "is-iter-end"
+    (is (is-iter-end [:iter-end "asdf"])))
+  (testing "All should return false on nil arg"
+    (is (= [false false false false]
+           (map #(% nil) [is-literal is-symbol
+                          is-iter-init is-iter-end])))))
+
+
+(deftest when-match-test
+  (testing "Assigns the result of matching the
+           string to to the symbol"
+    (is (= "f"
+           (when-match "foobar"
+             [match #"^f"] match))))
+  (testing "Returns the first match in order"
+    (is (= "pizza"
+           (when-match "pizzapie"
+             [match #"pizza"] match
+             [match #"pie"] match)))))
+
+(deftest tokenize-test
+  (testing "The empty program"
+    (is (= []
+           (tokenize ""))))
+  (testing "Simple literal tokenization"
+    (is (= [[:literal "lit"]]
+           (tokenize "lit"))))
+  (testing "iter-init tokenization"
+    (is (= [[:iter-init "{{#loop}}"]]
+           (tokenize "{{#loop}}"))))
+  (testing "iter-end tokenization"
+    (is (= [[:iter-end "{{/loop}}"]]
+           (tokenize "{{/loop}}"))))
+  (testing "Simple symbol tokenization"
+    (is (= [:literal :symbol :literal :iter-init
+            :literal :symbol :literal
+            :iter-end]
+           ;; the current implementation of tokenize sometimes separates literals instead of joining
+           ;; them into the same token. join-literals joins all conseq literals
+           (map first (join-literals (tokenize "foo {{a}} {{#loop}} asdf {{n}} {{/loop}}")))))))
+
+(deftest parse-test
+  (testing "The empty program"
+    (is (= []
+           (parse ""))))
+  (testing "A simple template with no iters"
+    (is (= [[:literal "My name is:"] [:symbol "name"] [:literal ", slim shady"]]
+           (parse "My name is:{{name}}, slim shady"))))
+  (testing "A template with an iter"
+    (is (= [[:literal "Your name is"] [:symbol "name"] [:literal " and your friends are "]
+            [:iter "friends" "," [ [:literal " "] [:symbol "name"] [:literal " "] ]]]
+           (parse "Your name is{{name}} and your friends are {{#friends , }} {{name}} {{#friends}}")))))
+
+
