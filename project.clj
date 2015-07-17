d4d4c231a6e78602ac47d60c0ff12e1c28bf1c11
fhur <fernando@authy.com>
2015-07-17 11:00:25 -0500

    Added description and project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject gabo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject gabo "0.1.1"
+  :description "A simple templating language for learning purposes"
+  :url "http://github.com/fhur/gabo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
